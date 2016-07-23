3f6768f44763a312c4860149919b61533ef7486a
Andrew Binns <abinns2011@my.fit.edu>
2016-07-23 16:51:23 -0400

    formatting tweaks on a tes

--- Test/src/MessageBuilderTest.java
+++ Test/src/MessageBuilderTest.java
@@ -9,8 +9,9 @@ public class MessageBuilderTest
 
 		b.add("normal text").startBold().add(" bolded ").startItalic().add("and normal").stopBold();
 		b.add("\nThis should be normal, after the bold and italc were stopped");
-		b.add("\nand now for...").startURL("http://google.com").add("some text in a url!").startBold().add("that is bolded!").stopBold().add("in only certain places").stopURL()
-				.add("with only the correct bits of the url being stopped.");
+		b.add("\nand now for...").startURL("http://google.com").add("some text in a url!");
+		b.startBold().add("that is bolded!").stopBold().add("in only certain places").stopURL();
+		b.add("with only the correct bits of the url being stopped.");
 
 		System.out.println(b.formatString(ForumMessageEncoder.DEBUG));
 		System.out.println(b.formatString(ForumMessageEncoder.PLAINTEXT));
