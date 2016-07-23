739eb1de99798d8ea1d15462a50b15d0f1bd90df
Andrew Binns <abinns2011@my.fit.edu>
2016-07-23 16:39:06 -0400

    First pass of new message builder paradigm

---
+++ GERTY Bot/src/werewolf/experimental/ForumMessageBuilder.java
@@ -0,0 +1,284 @@
+package werewolf.experimental;
+
+import java.awt.Color;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Set;
+
+public class ForumMessageBuilder
+{
+	private class Message extends Op
+	{
+		public Message(String msg)
+		{
+			super(null, msg);
+		}
+	}
+
+	private abstract class Op
+	{
+		private Style		style;
+		private String		msg;
+		private Object[]	params;
+
+		protected Op(Style s, String msg, Object... params)
+		{
+			this.style = s;
+			this.msg = msg;
+			this.params = params;
+		}
+
+		public String getMsg()
+		{
+			return this.msg;
+		}
+
+		public Object[] getParams()
+		{
+			return this.params;
+		}
+
+		public Style getStyle()
+		{
+			return this.style;
+		}
+	}
+
+	private class StartStyle extends Op
+	{
+		public StartStyle(Style s, Object... params)
+		{
+			super(s, null, params);
+		}
+	}
+
+	private class StopStyle extends Op
+	{
+		public StopStyle(Style s, Object... params)
+		{
+			super(s, null, params);
+		}
+	}
+
+	enum Style
+	{
+		QUOTE,
+		SPOILER,
+		STRIKE,
+		URL,
+		BOLD,
+		ITALIC,
+		COLOR,
+		HEADER,
+		UNDERLINE,
+		CODE,
+		LIST,
+		LISTITEM
+	}
+
+	private Set<Style>	currentlyActive;
+	private List<Op>	operations;
+
+	public ForumMessageBuilder()
+	{
+		this.currentlyActive = new LinkedHashSet<>();
+		this.operations = new ArrayList<>();
+	}
+
+	public ForumMessageBuilder append(String txt)
+	{
+		if (currentlyActive.contains(Style.LIST))
+			startStyle(Style.LISTITEM);
+		this.operations.add(new Message(txt));
+		if (currentlyActive.contains(Style.LIST))
+			stopStyle(Style.LISTITEM);
+		return this;
+	}
+
+	public ForumMessageBuilder startBold()
+	{
+		this.startStyle(Style.BOLD);
+		return this;
+	}
+
+	public ForumMessageBuilder startCodeBlock()
+	{
+		this.startStyle(Style.CODE);
+		return this;
+	}
+
+	public ForumMessageBuilder startColor(Color color)
+	{
+		this.startStyle(Style.BOLD, Integer.toString(color.getRGB() % 0xFFFFFF, 16));
+		return this;
+	}
+
+	public ForumMessageBuilder startHeader()
+	{
+		this.startStyle(Style.HEADER);
+		return this;
+	}
+
+	public ForumMessageBuilder startItalic()
+	{
+		this.startStyle(Style.ITALIC);
+		return this;
+	}
+
+	public ForumMessageBuilder startQuote(String author)
+	{
+		this.startStyle(Style.QUOTE, author);
+		return this;
+	}
+
+	public ForumMessageBuilder startSpoiler(String title)
+	{
+		this.startStyle(Style.SPOILER, title);
+		return this;
+	}
+
+	public ForumMessageBuilder startStrike()
+	{
+		this.startStyle(Style.STRIKE);
+		return this;
+	}
+
+	public ForumMessageBuilder startURL(String link)
+	{
+		this.startStyle(Style.BOLD, link);
+		return this;
+	}
+
+	public ForumMessageBuilder stopBold()
+	{
+		this.stopStyle(Style.BOLD);
+		return this;
+	}
+
+	public ForumMessageBuilder stopCodeBlock()
+	{
+		this.stopStyle(Style.CODE);
+		return this;
+	}
+
+	public ForumMessageBuilder stopColor()
+	{
+		this.stopStyle(Style.BOLD);
+		return this;
+	}
+
+	public ForumMessageBuilder stopHeader()
+	{
+		this.stopStyle(Style.HEADER);
+		return this;
+	}
+
+	public ForumMessageBuilder stopItalic()
+	{
+		this.stopStyle(Style.ITALIC);
+		return this;
+	}
+
+	public ForumMessageBuilder stopQuote()
+	{
+		this.stopStyle(Style.QUOTE);
+		return this;
+	}
+
+	public ForumMessageBuilder stopSpoiler()
+	{
+		this.stopStyle(Style.SPOILER);
+		return this;
+	}
+
+	public ForumMessageBuilder stopStrike()
+	{
+		this.stopStyle(Style.STRIKE);
+		return this;
+	}
+
+	public ForumMessageBuilder stopURL()
+	{
+		this.stopStyle(Style.BOLD);
+		return this;
+	}
+
+	public String formatString(ForumMessageEncoder encoder)
+	{
+		StringBuilder sb = new StringBuilder();
+
+		for (Op op : this.operations)
+		{
+			if (op instanceof StartStyle)
+				sb.append(encoder.getOpening(op.getStyle(), op.getParams()));
+			if (op instanceof StopStyle)
+				sb.append(encoder.getClosing(op.getStyle(), op.getParams()));
+			if (op instanceof Message)
+				sb.append(encoder.escape(op.getMsg()));
+		}
+		return sb.toString();
+	}
+
+	private void startStyle(Style s, Object... params)
+	{
+		if (this.currentlyActive.contains(s))
+			return;
+		this.currentlyActive.add(s);
+		this.operations.add(new StartStyle(s, params));
+	}
+
+	private void stopStyle(Style s)
+	{
+		if (!this.currentlyActive.contains(s))
+			return;
+		Iterator<Style> active = this.currentlyActive.iterator();
+		if (active.hasNext())
+		{
+			// Go up through our stack til we find style to stop
+			Style cur = active.next();
+			while (!cur.equals(s) && active.hasNext())
+				cur = active.next();
+			/*
+			 * Make a deque of all the styles to remove, in reverse order; that
+			 * is, the resulting deque should be in order of most recently added
+			 * to stack first.
+			 */
+			Deque<Style> toStop = new ArrayDeque<>(Style.values().length);
+			toStop.offerFirst(cur);
+			while (active.hasNext())
+				toStop.offerFirst(active.next());
+			// Add stop operations for each style
+			toStop.stream().map(style -> {
+				/*
+				 * To preserve the parameters from the start style, we must
+				 * iterate back along the op history til we find the starting
+				 * operator which starts the same style as the one we're trying
+				 * to close. Once we do, break out an return the stop style with
+				 * the appropriate params. Otherwise do one with just blank
+				 * params.
+				 */
+				Object[] p = new Object[]
+				{};
+				ListIterator<Op> li = this.operations.listIterator(this.operations.size());
+				while (li.hasPrevious())
+				{
+					Op prev = li.previous();
+					if (prev instanceof StartStyle)
+						if (prev.getStyle() == style)
+						{
+							p = prev.getParams();
+							break;
+						}
+				}
+				return new StopStyle(style, p);
+			}).forEach(this.operations::add);
+			// Remove the styles from the currently active set
+			toStop.forEach(this.currentlyActive::remove);
+		}
+	}
+}
--- GERTY Bot/src/werewolf/experimental/ForumMessageEncoder.java
+++ GERTY Bot/src/werewolf/experimental/ForumMessageEncoder.java
@@ -1,116 +1,119 @@
 package werewolf.experimental;
 
-import java.awt.Color;
-import java.util.logging.Logger;
+import java.util.HashMap;
+import java.util.Map;
 
-/**
- * This class handles messages passed from the game package. Classes that extend
- * this class implement functionality that translates rich text to a plaintext
- * output. For example, a BBCode implementation of this class might translate
- * bold text to become [b]text[/b] while an html implementation might translate
- * it to &lt;b&gt;text&lt;/b&gt;.
- * 
- * @author Michael Wells
- * @author Andrew Binns
- */
+import werewolf.experimental.ForumMessageBuilder.Style;
+
 public abstract class ForumMessageEncoder
 {
-	private static final Logger LOGGER = Logger.getLogger(ForumMessageEncoder.class.getName());
-
-	/**
-	 * The default implementation, which eliminates richtext. Suitable for
-	 * console or logfile output.
-	 */
-
-	public String encodeMessageElement(ForumMessageElement elem)
+	protected class Tags
 	{
-		return null;
+		private String	opening;
+		private String	closing;
+
+		public Tags(String opening, String closing)
+		{
+			this.opening = opening;
+			this.closing = closing;
+		}
+
+		public String getClosing()
+		{
+			return this.closing;
+		}
+
+		public String getOpening()
+		{
+			return this.opening;
+		}
 	}
-
-	protected abstract String encodeQuote(String msg, String author);
-
-	protected abstract String encodeSpoiler(String msg, String title);
-
-	protected abstract String encodeColor(String msg, Color color);
-
-	protected abstract String encodeUrl(String msg, String url);
-
-	protected abstract String encodeStrike(String msg);
-
-	protected abstract String encodeEscaped(String msg);
-
-	protected abstract String encodeBold(String msg);
-
-	protected abstract String encodeCodeblock(String msg);
-
-	protected abstract String encodeHeader(String msg);
-
-	protected abstract String encodeItalic(String msg);
 
 	public static final ForumMessageEncoder PLAINTEXT = new ForumMessageEncoder()
 	{
+		private Map<Style, Tags> map;
+
 		@Override
-		protected String encodeBold(String msg)
+		public String escape(String msg)
 		{
 			return msg;
 		}
 
 		@Override
-		protected String encodeCodeblock(String msg)
+		protected Map<Style, Tags> getTagMap()
 		{
-			return msg;
+			if (this.map == null)
+				this.initMap();
+			return this.map;
 		}
 
-		@Override
-		protected String encodeColor(String msg, Color color)
+		private void initMap()
 		{
-			return msg;
+			this.map = new HashMap<>();
+			this.map.put(Style.QUOTE, new Tags("Quote (%s):\n", ""));
+			this.map.put(Style.SPOILER, new Tags("[[%s]]:\n", ""));
+			this.map.put(Style.STRIKE, new Tags("", ""));
+			this.map.put(Style.URL, new Tags("", "<%s>"));
+			this.map.put(Style.BOLD, new Tags("", ""));
+			this.map.put(Style.ITALIC, new Tags("", ""));
+			this.map.put(Style.COLOR, new Tags("", ""));
+			this.map.put(Style.HEADER, new Tags("\n\n", "\n\n"));
+			this.map.put(Style.UNDERLINE, new Tags("", ""));
+			this.map.put(Style.CODE, new Tags("\n", "\n"));
+			this.map.put(Style.LIST, new Tags("--------", "--------"));
+			this.map.put(Style.LISTITEM, new Tags("(O)", "\n"));
 		}
-
-		@Override
-		protected String encodeHeader(String msg)
-		{
-			return msg.toUpperCase();
-		}
-
-		@Override
-		protected String encodeItalic(String msg)
-		{
-			return msg;
-		}
-
-		@Override
-		protected String encodeQuote(String msg, String author)
-		{
-			if (msg.contains("\n"))
-				return "Quote (" + author + "):\n\t" + msg.replaceAll("\n", "\n\t");
-			return "\"" + msg + "\" (" + author + ")";
-		}
-
-		@Override
-		protected String encodeSpoiler(String msg, String title)
-		{
-			return this.encodeHeader(title) + ":\n" + msg;
-		}
-
-		@Override
-		protected String encodeStrike(String msg)
-		{
-			return msg;
-		}
-
-		@Override
-		protected String encodeUrl(String msg, String url)
-		{
-			return msg + "<" + url + ">";
-		}
-
-		@Override
-		protected String escape(String msg)
-		{
-			return msg;
-		}
-
 	};
 
+	public static final ForumMessageEncoder DEBUG = new ForumMessageEncoder()
+	{
+		private Map<Style, Tags> map;
+
+		@Override
+		public String escape(String msg)
+		{
+			return msg;
+		}
+
+		@Override
+		protected Map<Style, Tags> getTagMap()
+		{
+			if (this.map == null)
+				this.initMap();
+			return this.map;
+		}
+
+		private void initMap()
+		{
+			this.map = new HashMap<>();
+			this.map.put(Style.QUOTE, new Tags("<START_QUOTE[%s]>", "<STOP_QUOTE>"));
+			this.map.put(Style.SPOILER, new Tags("<START_SPOILER[%s]>", "<STOP_SPOILER>"));
+			this.map.put(Style.STRIKE, new Tags("<START_STRIKE>", "<STOP_STRIKE>"));
+			this.map.put(Style.URL, new Tags("<START_URL[%s]>", "<STOP_URL>"));
+			this.map.put(Style.BOLD, new Tags("<START_BOLD>", "<STOP_BOLD>"));
+			this.map.put(Style.ITALIC, new Tags("<START_ITALIC>", "<STOP_ITALIC>"));
+			this.map.put(Style.COLOR, new Tags("<START_COLOR[%s]>", "STOP_COLOR"));
+			this.map.put(Style.HEADER, new Tags("START_HEADER", "STOP_HEADER"));
+			this.map.put(Style.UNDERLINE, new Tags("START_UNDERLINE", "STOP_UNDERLINE"));
+			this.map.put(Style.CODE, new Tags("START_CODE", "STOP_CODE"));
+			this.map.put(Style.LIST, new Tags("STOP_LIST", "START_LIST"));
+			this.map.put(Style.LISTITEM, new Tags("START_LISTITEM", "STOP_LISTITEM"));
+		}
+	};
+
+	public abstract String escape(String msg);
+
+	public String getClosing(Style style, Object... in)
+	{
+		String tag = this.getTagMap().get(style).getClosing();
+		return String.format(tag, in);
+	}
+
+	public String getOpening(Style style, Object... in)
+	{
+		String tag = this.getTagMap().get(style).getOpening();
+		return String.format(tag, in);
+	}
+
+	protected abstract Map<Style, Tags> getTagMap();
 }
